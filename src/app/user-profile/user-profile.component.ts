af00a2edc78f93cb5df07e8d07bec45e9f3f2711
Claudia Sanchez <sanchez.claudia001@gmail.com>
2018-08-10 23:09:15 -0400

    user profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  constructor( private myService: AuthService, private myRouter: Router ) { }
 
   ngOnInit() {
+    console.log('user in user prof component', this.user);
+
+
+    this.myService.isLoggedIn();
+
+
+    this.myService.currentUser
+    .subscribe((theUser) =>  {
+      console.log('user in user component', theUser);
+      this.user = theUser;
+    });
+
   }
 
 }
